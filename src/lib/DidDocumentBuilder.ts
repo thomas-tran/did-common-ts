1550312ad0755a3b0d05a34a78f52043a2c0d3a0
Thomas Tran <tldinteractive@gmail.com>
2023-04-24 11:23:51 +1000

    feat: added DID controller

--- src/lib/DidDocumentBuilder.ts
+++ src/lib/DidDocumentBuilder.ts
@@ -2,18 +2,34 @@ import { DID } from "./Did";
 import { DIDDocument } from "./DidDocument";
 
 export class DIDDocumentBuilder {
-  private didDocument: DIDDocument | undefined;
+  private didDocument: DIDDocument;
+
+  private constructor(did: DID) {
+    this.didDocument = new DIDDocument(did);
+  }
 
   public static createFromSubject(
     did: DID,
     controller?: DIDDocument
   ): DIDDocumentBuilder {
-    const builder = new DIDDocumentBuilder();
-    builder.didDocument = new DIDDocument(did);
+    const builder = new DIDDocumentBuilder(did);
 
     if (controller) {
       builder.didDocument.controllers = controller.controllers;
     }
     return builder;
   }
+
+  public addContext(context: string): DIDDocumentBuilder {
+    if(this.didDocument && this.didDocument.contexts == null) {
+      this.didDocument.contexts = [];
+    } 
+    
+    if (!this.didDocument.contexts.includes(context)) {
+      this.didDocument.contexts.push(context);
+    }
+    return this;
+  }
+
+
 }
